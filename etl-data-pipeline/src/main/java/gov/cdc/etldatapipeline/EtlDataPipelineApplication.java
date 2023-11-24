a40755243b9186d12067ec70afbe8a1801c5c761
AnandLoganathan <ukv1@cdc.gov>
2023-11-24 12:42:00 -0800

    CNDIT-796 : Data ETL Pipeline Kafka consumer initial checkin. Integrates with a given Kafka Topic populated by Debezium which monitors the ODSE table `NBS_Pages` Any changes made to the `NBS_Pages` will be picked up by Debezium and routed as data events in the topic `test.nbs_odse.dbo.NBS_page`
    The Data ETL Pipeline service will consume and log the data events.

---
+++ etl-data-pipeline/src/main/java/gov/cdc/etldatapipeline/EtlDataPipelineApplication.java
@@ -0,0 +1,13 @@
+package gov.cdc.etldatapipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtlDataPipelineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EtlDataPipelineApplication.class, args);
+    }
+
+}
---
+++ etl-data-pipeline/src/test/java/com/example/etldatapipeline/EtlDataPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.etldatapipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtlDataPipelineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
