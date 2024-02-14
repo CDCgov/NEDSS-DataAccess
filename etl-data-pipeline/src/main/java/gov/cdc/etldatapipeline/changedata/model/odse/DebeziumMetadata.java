1e0d2cf20b95b22826069dd4c8714f90d8502e0d
AnandLoganathan <ukv1@cdc.gov>
2024-02-14 14:48:10 -0800

    ```CNDIT-1024``` :  Convert Provider to kafka streams
    ```ChangeLog```:
    Add support for processing any change to the Provider module in ODSE:
    1. Accept list of PersonIds from input Kafka topic (cdc.nbs_odse.dbo.Provider)
    2. Process the incoming PersonIds into KafkaStream
    3. For each of the Person Id, fetch the corresponding Provider data
    4. Write the provider data to aggregated provider Kafka topic (cdc.nbs_odse.dbo.Provider.output)
    
    ```DataFlow:```
    ODSE.Person data event -> Kafka topic (cdc.nbs_odse.dbo.Provider) -> NEDSS-DataAcces Java Kafka Streaming Service -> AggregatedKafka topic (cdc.nbs_odse.dbo.Provider.output)
    
    ```Usage```: To trigger the Provider use case:
    `curl --location 'http://localhost:8090/provider' \
      --header 'Content-Type: application/json' \
      --data '[10000001,10000008]'`

--- etl-data-pipeline/src/main/java/gov/cdc/etldatapipeline/changedata/model/odse/DebeziumMetadata.java
+++ etl-data-pipeline/src/main/java/gov/cdc/etldatapipeline/changedata/model/odse/DebeziumMetadata.java
@@ -4,8 +4,12 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.Column;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
 
 @Data
+@ToString
+@EqualsAndHashCode
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class DebeziumMetadata {
     @Column(name = "ts_ms")
