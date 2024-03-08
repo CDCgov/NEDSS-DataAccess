e3c3c9d7a6034d54d0e19a5d5b9a386e3db9c04c
Ragul Shanmugam <rshanmugam@enquizit.com>
2024-03-08 11:34:22 -0800

    Created a basic skeleton for microservices

---
+++ etl-data-pipeline/status/src/main/java/gov/cdc/etldatapipeline/status/StatusApplication.java
@@ -0,0 +1,13 @@
+package gov.cdc.etldatapipeline.status;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StatusApplication.class, args);
+    }
+
+}
---
+++ etl-data-pipeline/status/src/test/java/gov/cdc/etldatapipeline/status/StatusApplicationTests.java
@@ -0,0 +1,13 @@
+package gov.cdc.etldatapipeline.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
