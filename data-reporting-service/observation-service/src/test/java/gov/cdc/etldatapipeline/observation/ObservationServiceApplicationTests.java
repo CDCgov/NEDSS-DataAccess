9a92975902d6bc56c6c1cee0b53895fd12fd1f67
Ragul Shanmugam <rshanmugam@enquizit.com>
2024-03-08 12:15:06 -0800

    Renamed project directory and removed provider module

---
+++ data-reporting-service/observation-service/src/test/java/gov/cdc/etldatapipeline/observation/ObservationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package gov.cdc.etldatapipeline.observation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObservationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
