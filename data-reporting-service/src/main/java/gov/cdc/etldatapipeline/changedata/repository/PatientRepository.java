4264ad59771f1f524bc1bd4184900f187ef343ba
AnandLoganathan <ukv1@cdc.gov>
2024-03-12 20:24:31 -0700

    `CNDIT-1067`: Forking the Patient and Provider stored procs as separate ones. Updating all the supporting data objects Added and verified the unit tests

--- data-reporting-service/src/main/java/gov/cdc/etldatapipeline/changedata/repository/PatientRepository.java
+++ data-reporting-service/src/main/java/gov/cdc/etldatapipeline/changedata/repository/PatientRepository.java
@@ -1,6 +1,6 @@
 package gov.cdc.etldatapipeline.changedata.repository;
 
-import gov.cdc.etldatapipeline.changedata.model.dto.PersonOp;
+import gov.cdc.etldatapipeline.changedata.model.dto.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -9,8 +9,8 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface PersonRepository extends JpaRepository<PersonOp, String>  {
-    @Query(nativeQuery = true, value = "execute sp_Person_Event :person_uids")
-    List<PersonOp> computeAllPatients(@Param("person_uids") String person_uids);
+public interface PatientRepository extends JpaRepository<Patient, String>  {
+    @Query(nativeQuery = true, value = "execute sp_Patient_Event :person_uids")
+    List<Patient> computePatients(@Param("person_uids") String person_uids);
 
 }
