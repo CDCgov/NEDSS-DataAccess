5bb069c24c32c75aeb575265d5fe324fdd5507aa
Ragul Shanmugam <rshanmugam@enquizit.com>
2024-04-01 12:23:19 -0700

    Refactored utility class

--- data-reporting-service/observation-service/src/main/java/gov/cdc/etldatapipeline/observation/service/ObservationService.java
+++ data-reporting-service/observation-service/src/main/java/gov/cdc/etldatapipeline/observation/service/ObservationService.java
@@ -1,15 +1,14 @@
 package gov.cdc.etldatapipeline.observation.service;
 
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import gov.cdc.etldatapipeline.observation.repository.IObservationRepository;
 import gov.cdc.etldatapipeline.observation.repository.model.Observation;
 import gov.cdc.etldatapipeline.observation.repository.model.ObservationTransformed;
+import gov.cdc.etldatapipeline.observation.util.ProcessObservationDataUtil;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.kstream.Consumed;
@@ -25,8 +24,8 @@ import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
-@Slf4j
 public class ObservationService {
+    private static final Logger logger = LoggerFactory.getLogger(ObservationService.class);
 
     @Value("${spring.kafka.stream.input.observation.topic-name}")
     private String observationTopic = "cdc.nbs_odse.dbo.Observation";
@@ -38,129 +37,42 @@ public class ObservationService {
     public String observationTopicOutputTransformed = "cdc.nbs_odse.dbo.Observation.output-transformed";
 
     private final IObservationRepository iObservationRepository;
-    private static final Logger logger = LoggerFactory.getLogger(ObservationService.class);
-
     private String topicDebugLog = "Received Observation ID: {} from topic: {}";
-
     private final KafkaTemplate<String, String> kafkaTemplate;
+    private final ProcessObservationDataUtil processObservationDataUtil;
+
 
     @Autowired
     public void processMessage(StreamsBuilder streamsBuilder) {
         streamsBuilder.stream(observationTopic, Consumed.with(Serdes.String(), Serdes.String()))
                 .filter((k, v) -> v != null)
-                .mapValues((key, value) -> {
-                    try {
-                        ObjectMapper objectMapper = new ObjectMapper();
-                        objectMapper.registerModule(new JavaTimeModule());
-                        JsonNode jsonNode = objectMapper.readTree(value);
-                        JsonNode payloadNode = jsonNode.get("payload");
-                        if (payloadNode != null && payloadNode.has("after")) {
-                            JsonNode afterNode = payloadNode.get("after");
-                            if (afterNode != null && afterNode.has("observation_uid")) {
-                                String observationUid = afterNode.get("observation_uid").asText();
-                                logger.debug(topicDebugLog, observationUid, observationTopic);
-                                Optional<Observation> observationData = iObservationRepository.computeObservations(observationUid);
-                                transformObservationData(observationData);
-                                return observationData.map(observation -> {
-                                    try {
-                                        return objectMapper.writeValueAsString(observation);
-                                    } catch (JsonProcessingException e) {
-                                        log.error("Error processing observation: {}", e.getMessage());
-                                        return null;
-                                    }
-                                }).orElse(null);
-                            }
-                        }
-                    } catch (Exception e) {
-                        log.error("Error processing observation: {}", e.getMessage());
-                    }
-                    return null;
-                })
+                .mapValues((key, value) -> processObservation(value))
                 .filter((key, value) -> value != null)
                 .to(observationTopicOutput, Produced.with(Serdes.String(), Serdes.String()));
     }
 
-    private void transformObservationData(Optional<Observation> observationData) {
-
-        observationData.ifPresent(data -> {
-            String personParticipationsJson = data.getPersonParticipations();
-            String organizationParticipationsJson = data.getOrganizationParticipations();
-            String materialParticipationsJson = data.getMaterialParticipations();
-            String followupObservationsJson = data.getFollowupObservations();
-            String observationDomainStatusCode = data.getObsDomainCdSt1();
-
-            ObservationTransformed observationTransformed = new ObservationTransformed();
-
+    private String processObservation(String value) {
+        try {
             ObjectMapper objectMapper = new ObjectMapper();
-            try {
-                JsonNode personParticipationsJsonArray = personParticipationsJson != null ? objectMapper.readTree(personParticipationsJson) : null;
-                JsonNode organizationParticipationsJsonArray = organizationParticipationsJson != null ? objectMapper.readTree(organizationParticipationsJson) : null;
-                JsonNode materialParticipationsJsonArray = materialParticipationsJson != null ? objectMapper.readTree(materialParticipationsJson) : null;
-                JsonNode followupObservationsJsonArray = followupObservationsJson != null ? objectMapper.readTree(followupObservationsJson) : null;
-                log.debug("Transformed object is: {}", observationTransformed);
-
-                if(personParticipationsJson != null && personParticipationsJsonArray.isArray()) {
-                    for(JsonNode jsonNode : personParticipationsJsonArray) {
-                        processJson(jsonNode, observationTransformed);
+            objectMapper.registerModule(new JavaTimeModule());
+            JsonNode jsonNode = objectMapper.readTree(value);
+            JsonNode payloadNode = jsonNode.get("payload");
+            if (payloadNode != null && payloadNode.has("after")) {
+                JsonNode afterNode = payloadNode.get("after");
+                if (afterNode != null && afterNode.has("observation_uid")) {
+                    String observationUid = afterNode.get("observation_uid").asText();
+                    logger.debug(topicDebugLog, observationUid, observationTopic);
+                    Optional<Observation> observationData = iObservationRepository.computeObservations(observationUid);
+                    if(observationData.isPresent()) {
+                        ObservationTransformed observationTransformed = processObservationDataUtil.transformObservationData(observationData.get());
+                        kafkaTemplate.send(observationTopicOutputTransformed, observationTransformed.toString());
                     }
+                    return objectMapper.writeValueAsString(observationData.get());
                 }
-                if(organizationParticipationsJson != null && organizationParticipationsJsonArray.isArray()) {
-                    for(JsonNode jsonNode : organizationParticipationsJsonArray) {
-                        processJson(jsonNode, observationTransformed);
-                    }
-                }
-                if(materialParticipationsJson != null && materialParticipationsJsonArray.isArray()) {
-                    for(JsonNode jsonNode : materialParticipationsJsonArray) {
-                        processJson(jsonNode, observationTransformed);
-                    }
-                }
-                if(followupObservationsJson != null && followupObservationsJsonArray.isArray()) {
-                    for(JsonNode jsonNode : followupObservationsJsonArray) {
-                        processJson(jsonNode, observationTransformed, observationDomainStatusCode);
-                    }
-                }
-            } catch (JsonProcessingException e) {
-                log.error("Error processing JSON array from observation data: {}", e.getMessage());
             }
-            kafkaTemplate.send(observationTopicOutputTransformed, observationTransformed.toString());
-        });
-    }
-
-    private void processJson(JsonNode jsonNode, ObservationTransformed observationTransformed) {
-        String typeCode = jsonNode.get("type_cd").asText();
-        String subjectClassCode = jsonNode.get("subject_class_cd").asText();
-
-        if(typeCode != null && subjectClassCode != null &&
-                typeCode.equals("ORD") && subjectClassCode.equals("PSN")) {
-            observationTransformed.setOrderingOrganizationId(jsonNode.get("entity_id").asLong());
+        } catch (Exception e) {
+            logger.error("Error processing observation: {}", e.getMessage());
         }
-        if (typeCode != null && subjectClassCode != null &&
-                typeCode.equals("PATSBJ") && subjectClassCode.equals("PSN")) {
-            observationTransformed.setPatientId(jsonNode.get("entity_id").asLong());
-        }
-        if (typeCode != null && subjectClassCode != null &&
-                typeCode.equals("PRF") && subjectClassCode.equals("ORG")) {
-            observationTransformed.setPerformingOrganizationId(jsonNode.get("entity_id").asLong());
-        }
-        if (typeCode != null && subjectClassCode != null &&
-                typeCode.equals("AUT") && subjectClassCode.equals("ORG")) {
-            observationTransformed.setAuthorOrganizationId(jsonNode.get("entity_id").asLong());
-        }
-        if (typeCode != null && subjectClassCode != null &&
-                typeCode.equals("ORD") && subjectClassCode.equals("ORG")) {
-            observationTransformed.setOrderingOrganizationId(jsonNode.get("entity_id").asLong());
-        }
-        if (typeCode != null && subjectClassCode != null &&
-                typeCode.equals("SPC") && subjectClassCode.equals("MAT")) {
-            observationTransformed.setMaterialId(jsonNode.get("entity_id").asLong());
-        }
-    }
-
-    private void processJson(JsonNode jsonNode, ObservationTransformed observationTransformed, String observationDomainStatusCode) {
-        String domainCdSt1 = jsonNode.get("domain_cd_st_1").asText();
-
-        if (observationDomainStatusCode.equalsIgnoreCase("Order") && domainCdSt1 != null && domainCdSt1.equals("Result")) {
-            observationTransformed.setResultObservationUid(jsonNode.get("result_observation_uid").asLong());
-        }
+        return null;
     }
 }
---
+++ data-reporting-service/observation-service/src/main/java/gov/cdc/etldatapipeline/observation/util/ProcessObservationDataUtil.java
@@ -0,0 +1,158 @@
+package gov.cdc.etldatapipeline.observation.util;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import gov.cdc.etldatapipeline.observation.repository.model.Observation;
+import gov.cdc.etldatapipeline.observation.repository.model.ObservationTransformed;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class ProcessObservationDataUtil {
+    private static final Logger logger = LoggerFactory.getLogger(ProcessObservationDataUtil.class);
+    public ObservationTransformed transformObservationData(Observation observation) {
+        ObservationTransformed observationTransformed = new ObservationTransformed();
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        String obsDomainCdSt1 = observation.getObsDomainCdSt1();
+
+        transformPersonParticipations(observation.getPersonParticipations(), obsDomainCdSt1, observationTransformed, objectMapper);
+        transformOrganizationParticipations(observation.getOrganizationParticipations(), obsDomainCdSt1, observationTransformed, objectMapper);
+        transformMaterialParticipations(observation.getMaterialParticipations(), obsDomainCdSt1, observationTransformed, objectMapper);
+        transformFollowupObservations(observation.getFollowupObservations(), obsDomainCdSt1, observationTransformed, objectMapper);
+
+        return observationTransformed;
+    }
+
+    private void transformPersonParticipations(String personParticipations, String obsDomainCdSt1, ObservationTransformed observationTransformed, ObjectMapper objectMapper) {
+        try {
+            JsonNode personParticipationsJsonArray = personParticipations != null ? objectMapper.readTree(personParticipations) : null;
+
+            if(personParticipations != null && personParticipationsJsonArray.isArray()) {
+                for(JsonNode jsonNode : personParticipationsJsonArray) {
+                    String typeCd = jsonNode.get("type_cd").asText();
+                    String subjectClassCd = jsonNode.get("subject_class_cd").asText();
+
+                    if(obsDomainCdSt1.equals("Order")) {
+                        if(typeCd != null && subjectClassCd != null) {
+                            if(typeCd.equals("ORD") && subjectClassCd.equals("PSN")) {
+                                observationTransformed.setOrderingPersonId(jsonNode.get("entity_id").asLong());
+                            }
+                            if (typeCd.equals("PATSBJ") && subjectClassCd.equals("PSN")) {
+                                observationTransformed.setPatientId(jsonNode.get("entity_id").asLong());
+                            }
+                        }
+                        else {
+                            logger.error("typeCd or subjectClassCd is null for the personParticipations: {}", personParticipations);
+                        }
+                    }
+                    else {
+                        logger.error("obsDomainCdSt1: {} is not valid", obsDomainCdSt1);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            logger.error("Error processing Person Participation JSON array from observation data: {}", e.getMessage());
+        }
+    }
+
+    private void transformOrganizationParticipations(String organizationParticipations, String obsDomainCdSt1, ObservationTransformed observationTransformed, ObjectMapper objectMapper) {
+        try {
+            JsonNode organizationParticipationsJsonArray = organizationParticipations != null ? objectMapper.readTree(organizationParticipations) : null;
+
+            if(organizationParticipations != null && organizationParticipationsJsonArray.isArray()) {
+                for(JsonNode jsonNode : organizationParticipationsJsonArray) {
+                    String typeCd = jsonNode.get("type_cd").asText();
+                    String subjectClassCd = jsonNode.get("subject_class_cd").asText();
+
+                    if(obsDomainCdSt1.equals("Result")) {
+                        if(typeCd != null && subjectClassCd != null) {
+                            if(typeCd.equals("PRF") && subjectClassCd.equals("ORG")) {
+                                observationTransformed.setPerformingOrganizationId(jsonNode.get("entity_id").asLong());
+                            }
+                        }
+                        else {
+                            logger.error("typeCd or subjectClassCd is null for the organizationParticipations: {}", organizationParticipations);
+                        }
+                    }
+                    if(obsDomainCdSt1.equals("Order")) {
+                        if(typeCd != null && subjectClassCd != null) {
+                            if(typeCd.equals("AUT") && subjectClassCd.equals("ORG")) {
+                                observationTransformed.setAuthorOrganizationId(jsonNode.get("entity_id").asLong());
+                            }
+                            if(typeCd.equals("ORD") && subjectClassCd.equals("ORG")) {
+                                observationTransformed.setOrderingOrganizationId(jsonNode.get("entity_id").asLong());
+                            }
+                        }
+                        else {
+                            logger.error("typeCd or subjectClassCd is null for the organizationParticipations: {}", organizationParticipations);
+                        }
+                    }
+                    else {
+                        logger.error("obsDomainCdSt1: {} is not valid", obsDomainCdSt1);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            logger.error("Error processing Organization Participation JSON array from observation data: {}", e.getMessage());
+        }
+    }
+
+    private void transformMaterialParticipations(String materialParticipations, String obsDomainCdSt1, ObservationTransformed observationTransformed, ObjectMapper objectMapper) {
+        try {
+            JsonNode materialParticipationsJsonArray = materialParticipations != null ? objectMapper.readTree(materialParticipations) : null;
+
+            if(materialParticipations != null && materialParticipationsJsonArray.isArray()) {
+                for(JsonNode jsonNode : materialParticipationsJsonArray) {
+                    String typeCd = jsonNode.get("type_cd").asText();
+                    String subjectClassCd = jsonNode.get("subject_class_cd").asText();
+
+                    if(obsDomainCdSt1.equals("Order")) {
+                        if(typeCd != null && subjectClassCd != null) {
+                            if(typeCd.equals("SPC") && subjectClassCd.equals("MAT")) {
+                                observationTransformed.setMaterialId(jsonNode.get("entity_id").asLong());
+                            }
+                        }
+                        else {
+                            logger.error("typeCd or subjectClassCd is null for the materialParticipations: {}", materialParticipations);
+                        }
+                    }
+                    else {
+                        logger.error("obsDomainCdSt1: {} is not valid", obsDomainCdSt1);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            logger.error("Error processing Material Participation JSON array from observation data: {}", e.getMessage());
+        }
+    }
+
+    private void transformFollowupObservations(String followupObservations, String obsDomainCdSt1, ObservationTransformed observationTransformed, ObjectMapper objectMapper) {
+        try {
+            JsonNode followupObservationsJsonArray = followupObservations != null ? objectMapper.readTree(followupObservations) : null;
+
+            if(followupObservations != null && followupObservationsJsonArray.isArray()) {
+                for (JsonNode jsonNode : followupObservationsJsonArray) {
+                    String domainCdSt1 = jsonNode.get("domain_cd_st_1").asText();
+
+                    if (obsDomainCdSt1.equals("Order")) {
+                        if (domainCdSt1 != null && domainCdSt1.equals("Result")) {
+                            observationTransformed.setResultObservationUid(jsonNode.get("result_observation_uid").asLong());
+                        }
+                        else {
+                            logger.error("domainCdSt1: {} is null or not valid for the followupObservations: {}", domainCdSt1, followupObservations);
+                        }
+                    }
+                    else {
+                        logger.error("obsDomainCdSt1: {} is not valid", obsDomainCdSt1);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            logger.error("Error processing Followup Observations JSON array from observation data: {}", e.getMessage());
+        }
+    }
+}
