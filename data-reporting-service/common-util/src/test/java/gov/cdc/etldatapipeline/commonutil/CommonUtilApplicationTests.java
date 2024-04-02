ed7d7db126e4accd0eb9daa1543d5053e800b78d
Ragul Shanmugam <rshanmugam@enquizit.com>
2024-04-02 11:34:45 -0700

    Added common-util module

---
+++ data-reporting-service/common-util/src/test/java/gov/cdc/etldatapipeline/commonutil/CommonUtilApplicationTests.java
@@ -0,0 +1,13 @@
+package gov.cdc.etldatapipeline.commonutil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonUtilApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
