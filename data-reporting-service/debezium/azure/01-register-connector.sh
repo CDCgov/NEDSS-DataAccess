ad336412acc5b7e49b2ef14733fe583c2c66ed53
AnandLoganathan <ukv1@cdc.gov>
2024-04-24 14:05:11 -0700

    CNDIT-1248 :  Debezium container deployment script to Azure ecosystem.

---
+++ data-reporting-service/debezium/azure/01-register-connector.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Strict mode, fail on any error
+set -euo pipefail
+
+export RESOURCE_GROUP="rtr-debezium"
+export CONTAINER_NAME="rtr-debezium"
+
+echo "finding debezium ip"
+export DEBEZIUM_IP=$(az container show -g $RESOURCE_GROUP -n $CONTAINER_NAME -o tsv --query "ipAddress.ip")
+
+echo "registering connector"
+curl -i -X POST \
+    -H "Accept:application/json" -H  "Content-Type:application/json" \
+    http://${DEBEZIUM_IP}:8083/connectors/ \
+    -d @../register-sqlserver-eh.json
