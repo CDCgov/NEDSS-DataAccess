c44915fe9e4a48cdafb9cdae915a565981f479c2
AnandLoganathan <ukv1@cdc.gov>
2024-04-30 23:51:53 -0700

    Liquibase updates :  Updating the liquibase run scripts to target ODSE and RDB databases

--- data-reporting-service/src/main/resources/db/db-general-001.sql
+++ data-reporting-service/src/main/resources/db/db-general-001.sql
@@ -3,15 +3,3 @@ use NBS_ODSE;
 -- Upgrade compatibility level to allow inbuilt functions such as StringSplit
 ALTER DATABASE NBS_ODSE SET COMPATIBILITY_LEVEL = 130;
 
--- Give user `sa` authorization to enable change data capture
-EXEC sp_changedbowner 'sa';
-
--- Enable Change Data Capture on the database
-EXEC sys.sp_cdc_enable_db;
-
--- Enable Change Data Capture on `Person` table
-EXEC sys.sp_cdc_enable_table
-     @source_schema = N'dbo',
-     @source_name = N'Person',
-     @role_name = NULL,
-     @supports_net_changes = 1;
